78e7333ba792ff14a22ba2499ebc4be922849996
Jason <jason.lm@live.com>
2019-08-30 05:31:33 -0700

    Initial Commit

---
+++ MyLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLayout
+//
+//  Created by Jason Lee on 8/30/19.
+//  Copyright © 2019 Jason Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
